a5b98a102cad7ede66e4eb268921e797e1f7498a
traderalwaysprofit <traderalwaysprofit@gmail.com>
2026-08-24 18:32:29 +0700

    build: include extracted Command Data V2 in production output

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -9,5 +9,7 @@ await mkdir(dist, { recursive: true });
 
 await cp(path.join(root, 'index.html'), path.join(dist, 'index.html'));
 await cp(path.join(root, 'src'), path.join(dist, 'src'), { recursive: true });
+await cp(path.join(root, 'data'), path.join(dist, 'data'), { recursive: true });
 
 console.log('Static production output created in dist/');
+console.log('Included: index.html, src/, data/');
