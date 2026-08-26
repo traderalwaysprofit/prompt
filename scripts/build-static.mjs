9c692652ae4b3c4ca250fd9c9b74ea3d6d84739c
traderalwaysprofit <traderalwaysprofit@gmail.com>
2026-08-26 10:55:44 +0700

    fix(build): include favicon in static output

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -8,8 +8,9 @@ await rm(dist, { recursive: true, force: true });
 await mkdir(dist, { recursive: true });
 
 await cp(path.join(root, 'index.html'), path.join(dist, 'index.html'));
+await cp(path.join(root, 'favicon.svg'), path.join(dist, 'favicon.svg'));
 await cp(path.join(root, 'src'), path.join(dist, 'src'), { recursive: true });
 await cp(path.join(root, 'data'), path.join(dist, 'data'), { recursive: true });
 
 console.log('Static production output created in dist/');
-console.log('Included: index.html, src/, data/');
+console.log('Included: index.html, favicon.svg, src/, data/');
